3c5ddb06eb259947a38888312bf8ab173c678223
Amirreza Farzan <amirfr2290@gmail.com>
2023-06-19 10:40:03 +0330

    Update lexroid.sh

--- lexroid.sh
+++ lexroid.sh
@@ -10,7 +10,6 @@ Green='\033[0;32m'
 RED='\033[0;31m'
 YEL='\033[0;33m'
 NC='\033[0m'
-platform=$2
 urlsfile=$1
 
 install_dependencies
@@ -19,13 +18,14 @@ install_dependencies
 
 complete -F _path_completion path
 
-# if path_handler.py printed the path then print storing location:
+# if data/path_handler.py printed the path then print storing location:
 if pth=$(python3 data/path_handler.py); then
     if [ -e $pth ]; then
         printf "${YEL}storing data in -> ${NC}${pth}\n"
     else
         mkdir $pth
     fi
+# if data/path_handler.py exited with error code 1 (sys.exit(1))
 elif [ $? -eq 1 ]; then
     printf "${RED}Please define a path for storing your recon result : ${NC}";
     read -e path;
@@ -54,54 +54,46 @@ fi
 
 printf "${Green}GATHERING SUBDOMAINS ${NC}\n"
 
-if [ -z "$urlsfile" ];
+if [ -z "$1" ];
         then printf "${RED}Please enter a domain name :\nUsage : subfind.sh <domain>\n"
         exit
 fi
 
-if [ -z "$platform" ];
-        then
-            printf "${RED}Please enter the platform name to download the whole subdomains from chaos${NC}\n"
-        else
-            wget "https://chaos-data.projectdiscovery.io/$platform.zip" -P /home/retrolex/recon/$urlsfile/$platform
-fi
+
+printf "${RED} GATHERING IP'S TO A FILE ${NC}\n"
+subfinder -silent -d $1 -nW -oI -o i.txt
+sleep 1
+cat i.txt | grep -E -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}' i.txt > $pth/ip.$1.txt
+rm i.txt
+
+
+assetfinder --subs-only $1 | httpx -silent -fr -http-proxy http://127.0.0.1:8080/ | anew $pth/sub.$1.txt
+findomain -t $1 -u temp.txt
+cat temp.txt | anew $pth/sub.$1.txt
+rm -rf temp.txt
+
+
+subfinder -d $1 -silent -all -o $1.txt
+cat $1.txt | anew $pth/sub.$1.txt
+rm -rf $1.txt
+
+amass enum -d $1 -active -cidr 1.2.3.4/24,4.3.2.1/24 -asn 12345 -o $1.txt
+cat $1.txt | anew $pth/sub.$1.txt
+rm -rf $1.txt
+
 
 printf "${Green}USING (subfinder, dnsx, httpx)${NC}\n"
-echo "$(subfinder -d $urlsfile -silent -all | dnsx -silent | httpx -silent -fr -http-proxy http://127.0.0.1:8080/ )"
+echo "$(subfinder -d $1 -silent -all | dnsx -silent | httpx -silent -fr -http-proxy http://127.0.0.1:8080/ )"
 
-printf "${RED} PUTTING SUB DOMAINS TO A FILE ${NC}\n"
-subfinder -silent -d $urlsfile -nW -oI -o i.txt
-sleep 1
-cat i.txt | grep -E -o '([0-9]{1,3}[\.]){3}[0-9]{1,3}' i.txt > $wind/PROJECTS/$urlsfile/ip.$urlsfile.txt
-
-assetfinder --subs-only $1 | httpx -silent -fr -http-proxy http://127.0.0.1:8080/ | anew $HOME/recon/$urlsfile/$urlsfile.txt
-findomain -t $1 -o
-
-cat $urlsfile.txt | anew $HOME/recon/$urlsfile/$urlsfile.txt
-
-rm -rf $urlsfile.txt
-
-subfinder -d $urlsfile -silent -all -o $urlsfile.txt
-
-cat $urlsfile.txt | anew $HOME/recon/$urlsfile/$urlsfile.txt
-
-rm -rf $urlsfile.txt
-
-amass enum -d $urlsfile -active -cidr 1.2.3.4/24,4.3.2.1/24 -asn 12345 -o $urlsfile.txt
-
-cat $urlsfile.txt | anew $HOME/recon/$urlsfile/$urlsfile.txt
-
-rm -rf $urlsfile.txt
 
 printf "${RED} FINDING LIVE HOSTS... ${NC}"
 echo ''
-cat $HOME/recon/$urlsfile/$urlsfile.txt | httprobe > $HOME/recon/$urlsfile/live.$urlsfile.txt
-printf "${RED} saved live hosts to live_subs ${NC}"
-echo ''
-printf "${RED} CHECKING FOR SUBDOMAIN TAKEOVER... ${NC}"
+cat $pth/sub.$1.txt | httprobe > $pth/live_sub.$1.txt
 
-printf "${RED} SENDING DOMAINS TO BURPSUITE ${NC}"
+
+printf "${RED} saved live hosts to live_subs live_sub.$1.txt ${NC}"
+echo ''
+
+printf "${RED} SENDING ALL DOMAINS TO BURPSUITE ${NC}"
 printf '\n'
-cat $HOME/recon/$urlsfile/$urlsfile.txt | httpx -silent -fr -http-proxy 'http://127.0.0.1:8080/'
-cp $HOME/recon/$urlsfile/live.$urlsfile.txt $wind/PROJECTS/$urlsfile/live.$urlsfile.txt
-cp $HOME/recon/$urlsfile/$urlsfile.txt $wind/PROJECTS/$urlsfile/$urlsfile.txt
+cat $pth/sub.$1.txt | httpx -silent -fr -http-proxy 'http://127.0.0.1:8080/'
